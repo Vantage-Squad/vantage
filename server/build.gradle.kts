e0ed64ed5cb0de9f3796326c755f238d7c8dc024
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-05-08 18:29:59 +0100

    Add server scaffold with .gitignore

---
+++ server/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(ktorLibs.plugins.ktor)
+}
+
+group = "com.example"
+version = "1.0.0-SNAPSHOT"
+
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+dependencies {
+    implementation(ktorLibs.server.callLogging)
+    implementation(ktorLibs.server.config.yaml)
+    implementation(ktorLibs.server.contentNegotiation)
+    implementation(ktorLibs.server.core)
+    implementation(ktorLibs.server.cors)
+    implementation(ktorLibs.server.netty)
+    implementation(ktorLibs.server.openapi)
+    implementation(ktorLibs.server.routingOpenapi)
+    implementation(ktorLibs.server.statusPages)
+    implementation(ktorLibs.server.swagger)
+    implementation(libs.logback.classic)
+
+    testImplementation(kotlin("test"))
+    testImplementation(ktorLibs.server.testHost)
+}
