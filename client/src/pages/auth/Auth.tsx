36297e807dee35b451abb104f9ba3a3a280a5ec8
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-05-10 08:36:28 +0100

    feat : initial frontend scaffolding.

---
+++ client/src/pages/auth/Auth.tsx
@@ -0,0 +1,10 @@
+
+const Auth = () => {
+    return (
+        <div className="min-h-screen flex items-center justify-center w-full text-white text-3xl font-bold">
+            This is the auth page.
+        </div>
+    );
+}
+
+export default Auth;
