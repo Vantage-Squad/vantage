ce347bc5b88d15f644c4c637295e7cf748068107
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-05-10 22:46:23 +0100

    feat : fine-tuned the axiosinstance and added necessary interceptors

--- client/src/lib/axiosInstance.ts
+++ client/src/lib/axiosInstance.ts
@@ -1,5 +1,5 @@
 import axios, { type InternalAxiosRequestConfig, type AxiosResponse, type AxiosError} from "axios"
-
+import { getToken } from "../services/auth";
 
 const axiosInstance = axios.create({
     baseURL : import.meta.env.VITE_API_BASE_URL,
@@ -7,6 +7,32 @@ const axiosInstance = axios.create({
     headers : {
         "Content-Type" : "application/json",
     }
-})
+});
+
+//attatch jwt to outgoing requests
+axiosInstance.interceptors.request.use(
+    (config : InternalAxiosRequestConfig) => {
+        const token = getToken();
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+//handles 401 unauthorized globally
+axiosInstance.interceptors.response.use(
+    (response : AxiosResponse) => response, 
+    async (error : AxiosError)=> {
+        if (error.response?.status === 401) {
+            if (!window.location.pathname.startsWith("/login")) {
+                const { logout } = await import("../services/auth");
+                await logout();
+            }
+        }
+        return Promise.reject(error);
+    }
+)
 
 export default axiosInstance ;
